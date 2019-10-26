dc23dbc7f638e20a83dae610d3ca184fba0214bf
Sean Hellum <seanhellum45@gmail.com>
2019-10-26 00:46:04 +0000

    format (again)

--- TicTacToe.java
+++ TicTacToe.java
@@ -130,7 +130,8 @@ public class TicTacToe {
         } else {
             getPlayerTypes();
         }
-        String[] pointless = {}; // Why is this line of code here? --- I don't know but it doesn't work without it
+        String[] pointless = {}; // Why is this line of code here? --- I don't know but it doesn't work without
+                                 // it
         return pointless;
     }
 
